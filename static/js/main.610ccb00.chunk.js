20bd166f7eadefb4dd6990d29e8a14b6d791766a
Alina Bovdyr <alinabovdyr92@gmail.com>
2021-01-14 15:47:19 +0200

    Updates

--- static/js/main.610ccb00.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-todolist"]=this["webpackJsonpmy-todolist"]||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var o=n(0),r=n(1),s=n.n(r),c=n(3);var i=function(){return Object(o.jsx)(o.Fragment,{})};n.n(c).a.render(Object(o.jsx)(s.a.StrictMode,{children:Object(o.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.610ccb00.chunk.js.map
